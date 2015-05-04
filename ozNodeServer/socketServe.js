9f05bfa444ad3a1996e8b49a900c974b229478bf
Meng Wei <weimeng@weimengs-MacBook-Air.local>
2015-05-04 01:11:45 -0400

    Update server for multiple clients test

--- ozNodeServer/socketServe.js
+++ ozNodeServer/socketServe.js
@@ -1,4 +1,6 @@
+
 var prompt = require('prompt');
+var fs = require('fs');
 var WebSocketServer = require('ws').Server;
 var socketServer = new WebSocketServer({port:14949});
 
@@ -7,20 +9,36 @@ var masterTime;
 
 var connectionCounter =0;
 var clients = [];
-
-
 var ofHost;
 
 socketServer.on('connection', function(client) {
 	clients[connectionCounter] = client;
-	connectionCounter++;
+	
     client.on('message', function(message) {
         console.log('received: %s', message);
         console.log('from: %s', clients.indexOf(client));
 		
 		var msgArray = message.split(' ');
-		
-		if(msgArray[0]=="/setScene"){
+
+		// on the first connection, every client will ask for a unique id from
+		// the server and send it back
+		// Server will write these ids into a file called 'message.txt'
+		if(msgArray[0]=="/myID"){
+    		var towrite = 'got client id: '+ msgArray[1]+'\n';
+			fs.appendFile('message.txt', towrite, function (err) {
+			  if (err) throw err;
+			});
+
+			// when the clients receive a message from server, they will send 
+			// back a message.
+			// server will write these message into the file.
+    	}else if(msgArray[0]=="/got"){
+	    		var towrite = 'client '+ msgArray[1]+' got message ~~~\n';
+				fs.appendFile('message.txt', towrite, function (err) {
+				  if (err) throw err;
+				});
+	    }
+		else if(msgArray[0]=="/setScene"){
 			setScene(message.split(' ')[1] );
 		} else if(msgArray[0] =="/getScene"){
 			client.send("/scene "+currentScene);
@@ -59,6 +77,9 @@ socketServer.on('connection', function(client) {
 		}
 		
     });
+
+	client.send('/yourID '+connectionCounter);
+	connectionCounter++;
     client.send('/welcome '+ currentScene);
     client.send('/time '+masterTime);
 });
@@ -66,21 +87,34 @@ socketServer.on('connection', function(client) {
 var date = new Date();
 var startTime = date.getTime();
 masterTime = startTime;
-
+var towrite = 'Here the server begins!\n';
+fs.writeFile('message.txt', towrite, function (err) {
+	if (err) throw err;
+});
 var update = function(){
 	date = new Date();
 	masterTime = date.getTime() - startTime;
 	socketServer.broadcast("/update "+ masterTime +" "+currentScene);
 	//console.log('sent: '+ date.getTime())
+	
 }
-
+function writeTextFile(filepath, output) {
+	var txtFile = new File(filepath);
+	txtFile.open("w"); //
+	txtFile.writeln(output);
+	txtFile.close();
+}
 var setScene = function(newScene){
-	if(currentScene != newScene){
+	if(newScene=='send'){
+		console.log("Server sent message");
+		socketServer.broadcast("/sendMyMsg Meng");
+
+	}else if(currentScene != newScene){
 		currentScene = newScene;
 		socketServer.broadcast("/sceneChange "+ currentScene);
 		console.log("Scene changed to: "+currentScene);
 		
-	} else{
+	}else{
 		console.log("Already in scene "+ newScene);
 	}
 	
@@ -108,6 +142,7 @@ function loadClientInfo(){
 
 
 socketServer.broadcast = function broadcast(data) {
+
   for(var i in this.clients) {
     this.clients[i].send(data);
   }
